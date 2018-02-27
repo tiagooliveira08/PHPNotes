b7caaac456bfd99db558321b4082d3c555c8a39a
xeromole <tiago_oliveira12@hotmail.com.br>
2018-02-27 18:45:47 -0300

    sessoes add

---
+++ section/section.php
@@ -0,0 +1,12 @@
+
+<?php
+//section e uma sessao criada entre o usuario e o servidor
+
+
+section_start(); // inicia a sessao com o tempo limite de mais ou menos 20 minutos
+
+$_SESSION["nome"] = "Tiago";
+
+
+
+?>
